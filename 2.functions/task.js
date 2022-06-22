fe118b0be0925f627e27366c1c0bbf1134173edc
Vitalii57 <vitalij.zubkov.2017@inbox.ru>
2022-06-22 21:36:48 +0300

    homework1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,65 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
+function getArrayParams([...arr]) {
+  let min, max, avg;
+  let sum = 0;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    min = Math.min(...arr);
+    max = Math.max(...arr);
+    sum += arr[i];
+    avg = sum / arr.length;
+  }
+  let obj = {
+    min: min,
+    max: max,
+    avg: Number(avg.toFixed(2)),
+  };
+  console.log(obj);
+  return obj;
 }
+getArrayParams([1, 2, 3, -100, 10]);
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+  return arr.reduce((acc, arrEl) => {
+    return acc + arrEl;
+  }, 0);
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = null;
+  arrOfArr.forEach((curArr) => {
+    const sum = func(curArr);
+    if (max === null || sum > max) {
+      max = sum;
+    }
+  });
   return max;
 }
 
+const testArr = [
+  [0, 0, 0],
+  [-1, -100],
+];
+
+console.log(makeWork(testArr, worker));
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    } else if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+
+  let difference = Math.abs(max - min);
+
+  return difference;
 }
+worker2([10, 10, 11], [20, 10]);
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result =
+    arr1.length == arr2.length &&
+    arr1.every(function (element, index) {
+      return element === arr2[index];
+    });
 
   return result; // boolean
 }
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
 
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+function advancedFilter([...arr]) {
+  let array = arr.filter(function (element) {
+    return element > 0 && element % 3 === 0;
+  });
+  let resultArr = array.map(function (element) {
+    return element * 10;
+  });
+  return resultArr;
 }
+console.log(advancedFilter([4, 3, 5]));
