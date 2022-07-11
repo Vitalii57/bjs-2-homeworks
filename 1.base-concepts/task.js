1d31cf9299a2b4078f63584fd36bfb0495c4f744
Vitalii57 <vitalij.zubkov.2017@inbox.ru>
2022-07-11 19:29:50 +0300

    taskOne

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,31 +1,25 @@
 "use strict";
-
 function solveEquation(a, b, c) {
   let arr = [];
-  let x0;
-  let x1;
-  let x2;
-  let m = [];
+  let x1, x2;
+
+  if (a === 0) {
+    return false;
+  }
+
   let D = b ** 2 - 4 * a * c;
+
   if (D < 0) {
-    arr.push(m);
+    return arr;
   } else if (D === 0) {
-    x0 = -b / (2 * a);
-    arr.push(x0);
+    x1 = -b / (2 * a);
+    arr[0] = x1;
   } else if (D > 0) {
     x1 = (-b + Math.sqrt(D)) / (2 * a);
-    arr.push(x1);
     x2 = (-b - Math.sqrt(D)) / (2 * a);
-    arr.push(x2);
+    arr[0] = x1;
+    arr[1] = x2;
   }
+
   return arr;
-}
-console.log(solveEquation(1, 5, 4));
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
