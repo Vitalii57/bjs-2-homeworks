4b3cc63c2092bfeca227f2b0671b76d6b06de76f
Vitalii57 <vitalij.zubkov.2017@inbox.ru>
2022-08-02 22:34:12 +0300

    task8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,62 @@
+// task_1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(".");
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func.call(this, ...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-
+// task_2
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timerID = null;
+  let repeatCall = false;
+
+  function wrapper(...rest) {
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      repeatCall = false;
+      return func.call(...rest);
+    });
+    if (!repeatCall) {
+      repeatCall = true;
+      func.call(...rest);
+    }
+  }
+  return wrapper;
 }
 
+// task_3
 function debounceDecorator2(func) {
-  // Ваш код
+  let timer = null;
+  let timerState = false;
+  wrapper.cout = 0;
+
+  function wrapper(...rest) {
+    ++wrapper.cout;
+    if (!timerState) {
+      func.call(this, ...rest);
+    }
+    timerState = true;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.call(this, ...rest);
+      timerState = false;
+    });
+  }
+  return wrapper;
 }
